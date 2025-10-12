787f2c0c713225391661d11f63c8847374153890
Isaac <isaaclanzoni99@gmail.com>
2025-10-12 17:33:04 -0300

    Update global-lessons.js

--- src/disciplines/lessons/global-lessons.js
+++ src/disciplines/lessons/global-lessons.js
@@ -2,10 +2,6 @@ document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
 
-function feedbackAlert(){
-    alert("Por enquanto essa função não está disponivel, o ADM ficou com preguiça de fazer, mas em breve vai implementar");
-}
-
 function redirect(n){
     if(n == 1){
         showLoading();
